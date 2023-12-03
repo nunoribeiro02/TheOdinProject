b3974b90e2c89aa75fd5de71d185dc8a8801c8fc
nunoribeiro02 <nuno.m.p.ribeiro@tecnico.ulisboa.pt>
2023-12-03 15:05:12 +0000

    feat: Temperature Conversion exercise

---
+++ foundations/javascript/fundamentals4/tempConversion.js
@@ -0,0 +1,13 @@
+const convertToCelsius = function(value) {
+    return Math.round((value - 32) * (5/9) * 10) /10;
+  };
+  
+  const convertToFahrenheit = function(value) {
+    return Math.round((value * 1.8 + 32) * 10) /10;
+  };
+  
+  // Do not edit below this line
+  module.exports = {
+    convertToCelsius,
+    convertToFahrenheit
+  };  
