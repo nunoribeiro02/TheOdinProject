37fa10ff3e618503339aa08f89efd8e657555533
nunoribeiro02 <nuno.m.p.ribeiro@tecnico.ulisboa.pt>
2023-12-16 01:42:01 +0000

    feat: buttons for etch-a-sketch

--- foundations/javascript/etch-a-sketch/etch-a-sketch.js
+++ foundations/javascript/etch-a-sketch/etch-a-sketch.js
@@ -1,10 +1,16 @@
-function createBoard(){
+function createBoard(maxRows, maxColumns){
     const board = document.querySelector(".board");
 
-    for (i = 0; i < 16; i++){
+    // Limmit is 100
+    if (maxRows > 100){
+        maxRows = 100;
+        maxColumns = 100;
+    }
+
+    for (i = 0; i < maxRows; i++){
         let div_row = document.createElement('div');
 
-        for (j = 0; j < 16; j++){
+        for (j = 0; j < maxColumns; j++){
             let div_column = document.createElement('div');
             div_column.style.backgroundColor = 'white';
             div_column.style.height = '25px';
@@ -16,5 +22,44 @@ function createBoard(){
     }   
 }
 
+//Create a 16x16 board
+createBoard(16, 16);
 
-createBoard();
+
+mode = "Normal";
+
+const btnNormal = document.querySelector(".normal");
+btnNormal.addEventListener('click', function (e) {
+    mode = "Normal";
+    e.target.style.backgroundColor = 'rgb(174, 170, 92)';
+    btnRainbow.style.backgroundColor = 'rgb(253, 248, 192)';
+    btnDarkning.style.backgroundColor = 'rgb(253, 248, 192)';
+
+});
+
+const btnRainbow = document.querySelector(".rainbow");
+btnRainbow.addEventListener('click', function (e) {
+    mode = "Rainbow";
+    e.target.style.backgroundColor = 'rgb(174, 170, 92)';
+    btnNormal.style.backgroundColor = 'rgb(253, 248, 192)';
+    btnDarkning.style.backgroundColor = 'rgb(253, 248, 192)';
+
+
+});
+
+const btnDarkning = document.querySelector(".darkening");
+btnDarkning.addEventListener('click', function (e) {
+    mode = "Darkening";
+    //e.target.style.backgroundColor = 'rgb(255, 248, 132)';
+    e.target.style.backgroundColor = 'rgb(174, 170, 92)';
+    btnNormal.style.backgroundColor = 'rgb(253, 248, 192)';
+    btnRainbow.style.backgroundColor = 'rgb(253, 248, 192)';
+
+});
+
+const btnGrid = document.querySelector(".grid");
+btnGrid.addEventListener('click', function () {
+    const userInput = prompt("Insert the size of the new grid (eg. 8, 10, 16)");
+    createBoard(userInput, userInput);
+
+});
