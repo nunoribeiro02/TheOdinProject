f28629d3d0c022019acf9891fcbc91c0e10359cc
nunoribeiro02 <nuno.m.p.ribeiro@tecnico.ulisboa.pt>
2024-03-02 18:58:16 +0000

    feat: form validation after submit button

--- intermediate/form/validate.js
+++ intermediate/form/validate.js
@@ -1,26 +1,39 @@
-console.log("Hello world!")
-
 let password = document.querySelector("#password");
-console.log("Hello !")
 let passwordC = document.querySelector("#password-confirm");
-console.log(" world!")
+let inputs = document.querySelectorAll("input")
+let btnSubmit = document.querySelector("#submit")
 
 password.addEventListener("input", verifyPassword);
 passwordC.addEventListener("input", verifyPassword);
 
+btnSubmit.addEventListener("click", validateSubmit);
+
 function verifyPassword(){
-    console.log("Verify")
+
     if (password.value == "" || passwordC.value == "") return;
 
     if (password.value == passwordC.value){ // Passwords Match
         password.classList.remove("error");
         passwordC.classList.remove("error");
-        console.log("MATHC tch")
-
     }
     else{ // Don't Match
-        console.log("dont match")
         password.classList.add("error");
         passwordC.classList.add("error");
+    }
+}
+
+
+function validateSubmit(){
+    console.log("CLICK")
+    for (let inpt of inputs){
+        if (!inpt.checkValidity() || (inpt.value == "" && inpt.hasAttribute('required'))){
+            console.log("one invalid")
+            inpt.classList.add("error");
+            
+            inpt.addEventListener("input", function validate(){
+                inpt.classList.remove("error");
+                inpt.removeEventListener("input", validate);
+            });
+        }
     }
 }
