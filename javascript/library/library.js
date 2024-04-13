2f443a5ad81bd7ed2b93df85921784075d61c38f
nunoribeiro02 <nuno.m.p.ribeiro@tecnico.ulisboa.pt>
2024-04-13 18:25:49 +0100

    feat: beggining of library project

---
+++ javascript/library/library.js
@@ -0,0 +1,16 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
+    this.info = function(){
+        return `${title} by ${author}, ${pages} pages, ${read ? 'read' : 'not read yet'}`;
+    }
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
